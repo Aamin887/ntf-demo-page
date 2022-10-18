ffdcec4ffcf66edd9a6088e4ef25e6b13cd4cd6c
Alhamin01 <forkahamin@yahoo.co.uk>
2022-10-18 11:43:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const menuBars = document.getElementById('bar');
+const menu = document.getElementById('menu');
+
+menuBars.addEventListener('click', () => {
+    if (menuBars.classList.contains('open-nav')) {
+        menuBars.classList.add('close-nav')
+        menuBars.classList.remove('open-nav')
+        menu.style.display = "flex"
+
+    }
+
+    else if (menuBars.classList.contains('close-nav')) {
+        menu.style.display = "none"
+        menuBars.classList.add('open-nav')
+        menuBars.classList.remove('close-nav')
+
+    }
+    console.log(11)
+})
